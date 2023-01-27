8c2d39610f921634e5d5835e3a2066a1619bd966
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-26 23:55:09 -0600

    hjg

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(void)
     char c='5';
     //char b='a';
     Configurar_PLL();  //Confiuracion de velocidad de reloj
-    Configurar_UART2();//Yo FCLK 20MHZ Baudrate 9600
+    Configurar_UART5();//Yo FCLK 20MHZ Baudrate 9600
 
     //Experimento 1
     //  Configurar_UART1(); //Jorge,Alberto,Navarro,Saul,Fabela -fclk 25MHZ Baud-rate 57600
