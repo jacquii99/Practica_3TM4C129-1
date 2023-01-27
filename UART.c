726eb202a944432d07976aa958541c3a6f7503ce
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-26 23:48:32 -0600

    uart5

--- UART.c
+++ UART.c
@@ -1,32 +1,35 @@
 #include "lib/include.h"
 
-extern void Configurar_UART0(void)
+extern void Configurar_UART5(void)
 {
-    SYSCTL->RCGCUART  = (1<<0);   //Paso 1 habilitar (RCGCUART) pag.344  
-    SYSCTL->RCGCGPIO |= (1<<0);     //Paso 2 (RCGCGPIO) habilitar el perifierico  pag.340 Enable clock port A
+    SYSCTL->RCGCUART  = (1<<5);   //Paso 1 habilitar (RCGCUART) para uart5 corresponde a PC6 Y PC7 pag.1164 
+    SYSCTL->RCGCGPIO |= (1<<2);     //Paso 2 (RCGCGPIO) habilitar el perifierico  pag.382  Enable clock port A
     //(GPIOAFSEL) pag.671 Habilitar la funcion alternativa 
-    GPIOA_AHB->AFSEL = (1<<1) | (1<<0);
-    //GPIO Port Control (GPIOPCTL) PA0-> U0Rx PA1-> U0Tx pag.688
-    GPIOA_AHB->PCTL = (GPIOA_AHB->PCTL&0xFFFFFF00) | 0x00000011;// (1<<0) | (1<<4);//0x00000011
+    GPIOC_AHB->AFSEL = (1<<6) | (1<<7); //Pin C6 Y C7 
+    //GPIO Port Control (GPIOPCTL) PC6-> U5Rx PC7-> U5Tx pag.688
+    GPIOC_AHB->PCTL = (GPIOA_AHB->PCTL&0x11FFFFFF) | 0x11000000;// (1<<0) | (1<<4);//0x00000011
+
     // GPIO Digital Enable (GPIODEN) pag.682
-    GPIOA_AHB->DEN = (1<<0) | (1<<1);//PA1 PA0
-    //UART0 UART Control (UARTCTL) pag.918 DISABLE!!
-    UART0->CTL = (0<<9) | (0<<8) | (0<<0);
+    GPIOC_AHB->DEN = (1<<6) | (1<<7);//PC6 Y PC7 
+    //UART5 UART Control (UARTCTL) pag.918 DISABLE!!
+    UART5->CTL = (0<<9) | (0<<8) | (0<<0);
 
     // UART Integer Baud-Rate Divisor (UARTIBRD) pag.914
     /*
-    BRD = 20,000,000 / (16 * 9600) = 130.2
+    BRD = 20,000,000 / (16 * 9600) = 130.2 
+    BRD0 40,000,000 / (16*28800)=86.80555556 
     UARTFBRD[DIVFRAC] = integer(0.2 * 64 + 0.5) = 14
+    UARTFBRD[DIVFRAC] = integer(0.80555556* 64 + 0.5) = 52.05
     */
-    UART0->IBRD = 130;
+    UART5->IBRD = 87;
     // UART Fractional Baud-Rate Divisor (UARTFBRD) pag.915
-    UART0->FBRD = 14;
+    UART5->FBRD = 52;
     //  UART Line Control (UARTLCRH) pag.916
-    UART0->LCRH = (0x3<<5)|(1<<4);
+    UART5->LCRH = (0x3<<5)|(1<<4);
     //  UART Clock Configuration(UARTCC) pag.939
-    UART0->CC =(0<<0);
+    UART5->CC =(0<<0);//para el reloj interno 
     //Disable UART0 UART Control (UARTCTL) pag.918
-    UART0->CTL = (1<<0) | (1<<8) | (1<<9);
+    UART5->CTL = (1<<0) | (1<<8) | (1<<9); //habilitar los pines
 
 
 
@@ -38,15 +41,15 @@ extern char readChar(void)
     //UART DR data 906
     int v;
     char c;
-    while((UART0->FR & (1<<4)) != 0 );
-    v = UART0->DR & 0xFF;
+    while((UART5->FR & (1<<4)) != 0 );
+    v = UART5->DR & 0xFF;
     c = v;
     return c;
 }
 extern void printChar(char c)
 {
-    while((UART0->FR & (1<<5)) != 0 );
-    UART0->DR = c;
+    while((UART5->FR & (1<<5)) != 0 );
+    UART5->DR = c;
 }
 extern void printString(char* string)
 {
@@ -85,11 +88,11 @@ extern char * readString(char delimitador)
 
 void nombre(char c)
 {
-    while ((UART0_FR & UART_FR_TXFF)!=0)
+    while ((UART5_FR & UART_FR_TXFF)!=0)
     UART0_DR_R=c;
 }
 void main (void)
 {
-    config_uart0();
+    config_uart5();
     nombre (); 
 } 
